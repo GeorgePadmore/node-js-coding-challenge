18cb783edf5df5a562353bdfd645e1d396ead348
George Padmore Yeboah <padmorey@gmail.com>
2022-02-15 12:43:36 +0000

    auth and user controller

---
+++ app_backend/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
+const { authService, userService, tokenService } = require('../services');
+
+const register = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.status(httpStatus.CREATED).send({ user, token: tokens.access.token, token_expires: tokens.access.expires, refresh_token: tokens.refresh.token, refresh_token_expires: tokens.refresh.expires });
+});
+
+const login = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authService.loginUserWithEmailAndPassword(email, password);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.send({ user, token: tokens.access.token, token_expires: tokens.access.expires, refresh_token: tokens.refresh.token, refresh_token_expires: tokens.refresh.expires });
+});
+
+const logout = catchAsync(async (req, res) => {
+  await authService.logout(req.body.refreshToken);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const refreshTokens = catchAsync(async (req, res) => {
+  const tokens = await authService.refreshAuth(req.body.refreshToken);
+  res.send({ ...tokens });
+});
+
+
+module.exports = {
+  register,
+  login,
+  logout,
+  refreshTokens
+};
